48b6a0069d0e8d1b772fac09ba33a93b06bc61cb
54maria-pia.even <mariapianavach@gmail.com>
2022-10-04 10:52:56 +0200

    Initial commit

---
+++ src/main/java/Oiseau.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+
+public class Oiseau extends Carre
+{
+    protected int vitesseVertical;
+
+    public int getVitesseVertical() {
+        return vitesseVertical;
+    }
+
+    public void setVitesseVertical(int vitesseVertical) {
+        this.vitesseVertical = vitesseVertical;
+    }
+
+    public void sauter() {
+
+    }
+
+    @Override
+    public void dessiner(Graphics2D dessin) {
+
+    }
+}
---
+++ src/main/java/Tuyau.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Tuyau extends Rectangle {
+    @Override
+    public void dessiner(Graphics2D dessin) {
+
+    }
+}
